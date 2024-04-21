2e6a0dba376a476ae44044685ebb8095ab77bd41
thebill21 <73658800+thebill21@users.noreply.github.com>
2024-04-21 01:44:08 -0700

    Delete current version

--- Week13Lab/src/main/java/cs3220/week13lab/PollRepository.java
+++
@@ -1,8 +0,0 @@
-package cs3220.week13lab;
-
-import cs3220.week13lab.model.Poll;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PollRepository extends JpaRepository<Poll, Integer> {
-    // Custom database queries can be defined here
-}
